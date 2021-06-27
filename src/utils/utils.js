bf334d7e092d0ad5c90175827c89b8d1d08feb60
Stefan So <s1126025@student.hsleiden.nl>
2021-06-27 02:54:02 +0200

    Added Constraint duration, Duration rounding
    If duration in the arguments is not provided, return 00:00.
    Convert incoming duration to a zero decimal number, to prevent formats like "01:54.54545".

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,11 @@
 export const formatSeconds = (duration) => {
-
+    if (!duration) {
+        return "00:00"
+    }
     function format(number) {
         return number.toString().padStart(2, "0");
     }
-    
+    duration = Math.round(duration);
     var minutes = Math.floor(duration / 60);
     var seconds = duration % 60;
 
