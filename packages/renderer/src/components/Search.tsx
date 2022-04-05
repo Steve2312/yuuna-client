34789e70d28c92b17f848733829bc3d8ae6a192a
Stefan So <34347838+Steve2312@users.noreply.github.com>
2022-04-05 23:24:12 +0200

    Added Titlebar

--- packages/renderer/src/components/Search.tsx
+++ packages/renderer/src/components/Search.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef } from "react";
 import Banner from "@/components/Banner";
 import SearchBanner from "../assets/banners/search.png"
 import SearchMenu from "@/components/SearchMenu";
@@ -7,6 +7,7 @@ import SearchCard from "@/components/SearchCard";
 import styles from "@/styles/search.module.scss";
 import SearchService from "@/services/SearchService";
 import List from "@/components/List";
+import TitleBar from "@/components/TitleBar";
 
 const Search: React.FC = () => {
 
@@ -17,48 +18,43 @@ const Search: React.FC = () => {
         SearchService.search('', 'ranked');
     }, [])
 
-    // const backToTop = () => {
-    //     const listElement = listRef.current;
-    //     if (listElement) listElement.scrollTo({
-    //         top: 0,
-    //         behavior: 'smooth'
-    //     })
-    // }
-
     return (
-        <List
-            ref={listRef}
-            header={
-                () => {
-                    return (
-                        <>
-                            <Banner title="Search" src={SearchBanner} scrollableElementRef={listRef}/>
-                            <SearchMenu />
-                        </>
-                    )
+        <>
+            <TitleBar title="Search" src={SearchBanner} scrollableElementRef={listRef} />
+            <List
+                ref={listRef}
+                header={
+                    () => {
+                        return (
+                            <>
+                                <Banner title="Search" src={SearchBanner} scrollableElementRef={listRef}/>
+                                <SearchMenu />
+                            </>
+                        )
+                    }
                 }
-            }
-            data={search.results.beatmaps}
+                data={search.results.beatmaps}
 
-            keyExtractor={data => data.id}
+                keyExtractor={data => data.id}
 
-            render={
-                ({data, index}) => {
-                    return (
-                        <SearchCard beatmap={data} index={index}/>
-                    )
+                render={
+                    ({data, index}) => {
+                        return (
+                            <SearchCard beatmap={data} index={index}/>
+                        )
+                    }
                 }
-            }
 
-            prerenderCount={7}
-            componentHeight={90}
-            onEndReached={() => {
-                if (!search.results.lastPage) {
-                    SearchService.searchNext();
-                }
-            }}
-            thresholdEnd={3000}
-        />
+                prerenderCount={7}
+                componentHeight={90}
+                onEndReached={() => {
+                    if (!search.results.lastPage) {
+                        SearchService.searchNext();
+                    }
+                }}
+                thresholdEnd={3000}
+            />
+        </>
     );
 }
 
---
+++ packages/renderer/src/components/TitleBar.tsx
@@ -0,0 +1,77 @@
+import React, {useEffect, useState} from "react";
+import styles from "@/styles/titlebar.module.scss";
+import clamp from "@/utils/Clamp";
+
+type Props = {
+    title: string,
+    src: string,
+    scrollableElementRef: React.RefObject<HTMLDivElement>
+}
+
+const TitleBar: React.FC<Props> = ({title, src, scrollableElementRef}) => {
+
+    const [scrollTop, setScrollTop] = useState<number>(0);
+    const [firstElementOffsetBottom, setFirstElementOffsetBottom] = useState<number>(0);
+
+    const headerImage = {
+        backgroundImage: `linear-gradient(var(--banner-overlay), var(--banner-overlay)), url(${src})`,
+    }
+
+    useEffect(() => {
+        const scrollableElement = scrollableElementRef?.current;
+
+        if (scrollableElement) {
+            updateOffsetFirstElement();
+            scrollableElement.addEventListener("scroll", updateScrollTop);
+            window.addEventListener("resize", updateOffsetFirstElement);
+            return () => {
+                scrollableElement.removeEventListener("scroll", updateScrollTop);
+                window.removeEventListener("resize", updateOffsetFirstElement);
+            }
+        }
+    }, [scrollableElementRef])
+
+    const updateScrollTop = (event: Event) => {
+        const element = event.target as HTMLDivElement;
+        setScrollTop(element.scrollTop);
+    }
+
+    const updateOffsetFirstElement = () => {
+        const firstElement = scrollableElementRef.current?.firstElementChild as HTMLDivElement;
+        if (firstElement) {
+            const firstElementOffsetBottom = firstElement.offsetTop + firstElement.offsetHeight;
+            setFirstElementOffsetBottom(firstElementOffsetBottom);
+        }
+    }
+
+    const backToTop = () => {
+        const scrollableElement = scrollableElementRef.current;
+        if (scrollableElement) scrollableElement.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+
+    const titleBarOpacity = firstElementOffsetBottom ? (scrollTop - firstElementOffsetBottom + 95) / 70 : 0
+    const titleBarStyle = {
+        display: titleBarOpacity < 0 ? "none" : "flex",
+        opacity: clamp(titleBarOpacity, 0, 1)
+    }
+
+    const titleBarItemOpacity = scrollTop > firstElementOffsetBottom - 50 ? 1 : 0;
+    const titleBarItemTransformY = scrollTop > firstElementOffsetBottom - 50 ? 0 : 50;
+    const titleBarItemStyle = {
+        opacity: titleBarItemOpacity,
+        transform: 'translateY(' + titleBarItemTransformY + '%)',
+    }
+
+    return (
+        <div className={styles.titleBar} style={titleBarStyle}>
+            <span className={styles.title} style={titleBarItemStyle}>{title}</span>
+            <span className={styles.backToTop} style={titleBarItemStyle} onClick={backToTop}>Back to top</span>
+            <div className={styles.titleBarImage} style={headerImage}/>
+        </div>
+    );
+}
+
+export default TitleBar;
