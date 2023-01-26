946467fb84f55d4c0fab201c1d1922776adfccea
Stefan So <stefanso2312@gmail.com>
2023-01-27 00:20:11 +0100

    Add library search

--- src/renderer/components/Library.tsx
+++ src/renderer/components/Library.tsx
@@ -1,15 +1,22 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import Banner from '@/components/Banner'
 import LibraryBanner from '../assets/banners/88738718_p0.jpg'
 import useLibraryService from '@/hooks/useLibraryService'
 import List from '@/components/List'
 import TitleBar from '@/components/TitleBar'
 import LibraryCard from '@/components/LibraryCard'
+import LibraryMenu from '@/components/LibraryMenu'
 
 const Library: React.FC = () => {
 
     const listRef = useRef<HTMLDivElement>(null)
+    const [input, setInput] = useState<string>('')
     const [library] = useLibraryService()
+
+    const songs = library.songs.filter(song => {
+        const keywords = [song.title, song.artist, song.source, song.creator, song.version].join(' ').toUpperCase()
+        return keywords.includes(input.toUpperCase())
+    })
 
     return (
         <>
@@ -21,11 +28,12 @@ const Library: React.FC = () => {
                         return (
                             <>
                                 <Banner title="Library" src={LibraryBanner} scrollableElementRef={listRef}/>
+                                <LibraryMenu onInput={setInput} />
                             </>
                         )
                     }
                 }
-                data={library.songs}
+                data={songs}
 
                 keyExtractor={data => data.id}
 
